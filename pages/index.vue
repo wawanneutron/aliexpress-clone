6a60c0dd3f53858a66221fd7086b078f44ac06a0
wawanneutron <wawanneutron1331@gmail.com>
2024-03-10 16:37:06 +0700

    layouts

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <MainLayout>
+    <!-- <div id="IndexPage" class="mt-4 max-2-[1200px] mx-auto px-2"></div> -->
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '@/layouts/MainLayout.vue'
+</script>
