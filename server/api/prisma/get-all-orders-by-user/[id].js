c009e07020a777cfce39d36cb61e82d68ec7440d
wawanneutron <wawanneutron1331@gmail.com>
2024-03-20 23:43:42 +0700

    get data api with prisma server

---
+++ server/api/prisma/get-all-orders-by-user/[id].js
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+  let res = await prisma.addresses.findMany({
+    where: { userId: event.context.params.id }
+  })
+
+  return res
+})
